e495f70d8e57c21dc8a536bd7194c3101010cf2c
hisamuddin01 <hisamuddin357545@gmail.com>
2023-09-25 13:14:12 +0530

    dark mode activated

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let settingsMenu = document.querySelector(".settings-menu");
+const settingsMenu = document.querySelector(".settings-menu");
+const darkBtn = document.getElementById('dark-btn');
 
 function settingsMenuToggle() {
     settingsMenu.classList.toggle("settings-menu-height");
 }
+// darkBtn.onclick = function() {
+//     darkBtn.classList.toggle("dark-btn-on")
+//     document.body.classList.toggle("dark-theme")
+// }
+darkBtn.addEventListener("click", ()=> {
+    darkBtn.classList.toggle("dark-btn-on")
+    document.body.classList.toggle("dark-theme")
+})
